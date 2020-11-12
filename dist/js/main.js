060662a8d1e7106b67c11b2b5411439f3dc6a5b6
GodfreySam <godfreysam09@gmail.com>
2020-11-12 02:39:16 +0100

    upgraded portfolio

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+menuBtn.addEventListener("click", function () {
+  // set initial state of Menu
+  let showMenu = false;
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+  }
+  // set Menu State
+  menuBtn.onclick = function () {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+    showMenu = false;
+      window.setTimeout(function () {
+        window.location.reload();
+      }, 500);
+  };
+});
