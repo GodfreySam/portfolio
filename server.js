e6bc42c448b161d752f644a025337450edbb6e3b
GodfreySam <godfreysam09@gmail.com>
2021-10-14 03:57:47 +0100

    updated

--- server.js
+++ server.js
@@ -171,4 +171,4 @@ app.post("/contact", (req, res)=> {
 
 const PORT = process.env.PORT || 3050;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
