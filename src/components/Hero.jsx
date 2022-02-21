d6507ea0453afd0ae6049fc435d6f52749689d7f
GodfreySam <godfreysam09@gmail.com>
2022-02-21 12:58:21 +0100

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import "./hero.css";
 
 const Hero = () => {
 	return (
 		<section id="colorlib-hero" className="js-fullheight" data-section="home">
 			<div className="flexslider js-fullheight">
 				<ul className="slides">
-					<li style={{ backgroundImage: "url(images/img_bg_1.jpg)" }}>
+					<li className="slides-bg">
+						<div className="slide-overlay"></div>
 						<div className="overlay"></div>
 						<div className="container-fluid">
 							<div className="row">
 								<div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 									<div className="slider-text-inner js-fullheight">
-										<div className="desc">
+										<div className="info-desc">
 											<h1>
 												Hi! <br />
 												I'm Godfrey
 											</h1>
 											<p>
 												<a
-													href="https://drive.google.com/file/d/1nhacd3r6hBbqmVJfRibT6dHgqgYXIRLe/view?usp=sharing"
+													href="https://docs.google.com/document/d/1RRiHDipItQe17C7DNc1aKMWuhWo5LI_YwK_VVARc3fU/edit?usp=sharing"
 													target="_blank"
 													rel="noopener noreferrer"
 													className="btn btn-primary btn-learn"
@@ -32,13 +34,13 @@ const Hero = () => {
 							</div>
 						</div>
 					</li>
-					<li style={{ backgroundImage: "url(images/img_bg_1.jpg)" }}>
+					<li className="slides-bg">
 						<div className="overlay"></div>
 						<div className="container-fluid">
 							<div className="row">
 								<div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 									<div className="slider-text-inner">
-										<div className="desc">
+										<div className="info-desc">
 											<h1>
 												I am a <br />
 												Full Stack Developer
@@ -55,6 +57,9 @@ const Hero = () => {
 						</div>
 					</li>
 				</ul>
+				<div className="hero-img">
+					<img src="images/godfrey.png" alt="" />
+				</div>
 			</div>
 		</section>
 	);
